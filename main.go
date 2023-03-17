c5bf8554c09c9e7f0fba93f721378e7f90e73f87
Adrian Cole <adrian@tetrate.io>
2023-03-17 15:42:03 +0800

    deps: updates wazero to 1.0.0-rc.2
    This updates [wazero](https://wazero.io/) to [1.0.0-rc.2](https://github.com/tetratelabs/wazero/releases/tag/v1.0.0-rc.2) which notably
    * is the final release candidate before 1.0 next Friday.
    * improves instantiation performance (startup time)
    * passes `os` package tests defined by Go.
    * no longer supports importing unnamed modules.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/tetratelabs/wazero"
-	"github.com/tetratelabs/wazero/experimental"
-	gojs "github.com/tetratelabs/wazero/imports/go"
+	"github.com/tetratelabs/wazero/experimental/gojs"
 	"github.com/tetratelabs/wazero/sys"
 )
 
@@ -17,14 +16,17 @@ func main() {
 	log.SetOutput(os.Stderr)
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
-	ctx, err := experimental.WithCompilationCacheDirName(
-		ctx,
-		filepath.Join(os.TempDir(), "wazero", "buildcache"),
-	)
+
+	cacheDir := filepath.Join(os.TempDir(), "wazero", "buildcache")
+	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
 	if err != nil {
 		log.Fatal("Failed to create build cache:", err)
 	}
-	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig())
+
+	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig().
+		WithCloseOnContextDone(true).
+		WithCompilationCache(cache))
+
 	binPath := "./wasm/" + os.Args[0] + ".wasm"
 	bin, err := os.ReadFile(binPath)
 	if err != nil {
@@ -34,8 +36,16 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to compile module:", err)
 	}
-	conf := wazero.NewModuleConfig().WithStdin(os.Stdin).WithStderr(os.Stderr).WithStdout(os.Stdout).WithArgs(os.Args...)
-	if err := gojs.Run(ctx, r, m, conf); err != nil {
+
+	gojs.MustInstantiate(ctx, r)
+
+	conf := wazero.NewModuleConfig().
+		WithStdin(os.Stdin).
+		WithStderr(os.Stderr).
+		WithStdout(os.Stdout).
+		WithArgs(os.Args...)
+
+	if err := gojs.Run(ctx, r, m, gojs.NewConfig(conf)); err != nil {
 		exitErr, ok := err.(*sys.ExitError)
 		if !ok {
 			log.Fatal("Failed to execute binary:", err)
